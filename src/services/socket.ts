94195786b530c70c097f3d36ff611d6c8651a16b
Corentin Deleuse <dlse.corentin@gmail.com>
2024-06-06 21:04:19 +0200

    chore: update server side url socket

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000', {
+const socket = io('https://fight-socket-backend-1.onrender.com/', {
     withCredentials: true,
   });
 
