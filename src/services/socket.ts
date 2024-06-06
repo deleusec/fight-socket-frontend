d7113822e7f8aa7e85285b7b8d74b15f783e686c
Corentin Deleuse <dlse.corentin@gmail.com>
2024-06-06 22:40:24 +0200

    feat: change location assets

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('https://fight-socket-backend-1.onrender.com', {
+const socket = io('https://fight-socket-backend-1.onrender.com/', {
     withCredentials: true,
   });
 
